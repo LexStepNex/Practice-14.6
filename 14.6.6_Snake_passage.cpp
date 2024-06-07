fa5817cb2e8f391afecf5b0ff750f4f81fc8c0c0
Alexandr <sasha_stepanov94@mail.ru>
2024-06-07 08:57:26 +0300

    Sixth

---
+++ 14.6.6_Snake_passage.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+void printArray(int arr[][5]) {
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 5; j++) {
+      std::cout << arr[i][j] << " ";
+    }
+    std::cout << "\n";
+  }
+}
+
+int main() {
+  int arr[5][5];
+
+  int n = 0;
+
+  for (int i = 0; i < 5; i++) {
+    int m = (0 - i % 2);
+    for (int j = 0; j < 5; j++) {
+      arr[i][(j + j * m) - ((4 - j) * m)] = n;
+      n++;
+    }
+  }
+
+  printArray(arr);
+}
